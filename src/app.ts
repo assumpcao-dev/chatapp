90648645aab0f95cbef5dc35694c0433182f543e
Rafael Assumpcao dos santos <assumpcao@Rafaels-MBP.lan>
2021-04-19 11:37:53 -0300

    First Commit - Some configurations to run our server

---
+++ src/app.ts
@@ -0,0 +1,22 @@
+import express, { NextFunction, Request, Response } from 'express';
+import AppError from './shared/errors/AppError';
+import 'dotenv/config';
+
+const app = express();
+
+app.use(express.json());
+
+app.use((error: Error, request: Request, response: Response, next: NextFunction) => {
+  if (error instanceof AppError) {
+    return response.status(error.statusCode).json({
+      status: 'error',
+      message: error.message,
+    })
+  }
+  return response.status(500).json({
+    status: 'error',
+    message: 'Internal Server Error',
+  })
+});
+
+app.listen(process.env.PORT, () => console.log('Server running: ' + process.env.PORT));
